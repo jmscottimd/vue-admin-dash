69daf13d769568312864b49de54332f7b2d460c9
jmscottimd <jmscottimd@protonmail.com>
2020-02-02 18:23:22 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
@@ -9,23 +9,33 @@
 </template>
 
 <style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
+body {
+  background: $dark-blue;
+}
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold($purple);
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: $system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $white;
 }
-
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
-      color: #42b983;
+      color: $white;
     }
   }
 }
